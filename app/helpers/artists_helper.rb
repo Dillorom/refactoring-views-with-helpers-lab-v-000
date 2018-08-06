a0718981ee0f3609a5596f63b20110e76c080337
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-08-06 22:09:33 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,11 +1,3 @@
 module ArtistsHelper
-  def display_artist(song)
-    if song.artist.name.empty?
-      <%= link_to "Add Artist", song_edit_path %>
-    else
-      <%= link_to "Artist", artists_path %>
 
-    end
-
-  end
 end
